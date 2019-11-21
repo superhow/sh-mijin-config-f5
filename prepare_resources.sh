928f1f39760609e66d7fb3a1f8eba29e5a712a5c
Vytautas Superhero <vytautas@techsuperheroes.ch>
2019-11-21 18:59:31 +0200

    Minor fix

--- prepare_resources.sh
+++ prepare_resources.sh
@@ -62,9 +62,9 @@ function prepare_base_resources() {
 
 	local -A network_pairs=(
 		"generationHash" "$generation_hash"
-        "publicKey" "$public_key"
-        "totalChainImportance" "17'000'000"
-        "initialCurrencyAtomicUnits" "1'079'999'998'000'000")
+		"publicKey" "$public_key"
+		"totalChainImportance" "17'000'000"
+		"initialCurrencyAtomicUnits" "1'079'999'998'000'000")
 	run_sed "network" network_pairs
 
 	local -A user_pairs=(
