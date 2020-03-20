a52807c45ce1402d338eabc93e4239d4ca7e6ea0
Vytautas K <ministras@users.noreply.github.com>
2020-03-20 16:58:03 +0200

    new mongo reset

--- reset_mongo.sh
+++ reset_mongo.sh
@@ -4,6 +4,7 @@ catapult_source=$1
 
 cd ${catapult_source}/scripts/mongo
 echo "dropping mongo ..."
-mongo "${REMOTE_MONGODB_HOST:-127.0.0.1:27017}"/catapult < mongoDbDrop.js
+mongo catapult < mongoDbDrop.js
+
 echo "preparing mongo ..."
-mongo "${REMOTE_MONGODB_HOST:-127.0.0.1:27017}"/catapult < mongoDbPrepare.js
+mongo catapult < mongoDbPrepare.js
